d9af4c1a6bf2bc1a07b6ba51c0d3f24dd0242932
Gabriel Borel <biel_borel@hotmail.com>
2023-06-08 04:35:05 -0300

    test: ensure SignUpController returns an error if no email is provided

--- src/presentations/controllers/signup.spec.ts
+++ src/presentations/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe("SignUp Controller", () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new Error("name is required"));
   });
+
+  test("should return 400 if no email is provided", () => {
+    const sut = new SignUpController();
+    const request = {
+      body: {
+        name: "any_name",
+        email: "",
+        password: "any_password",
+      },
+    };
+    const response = sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error("email is required"));
+  });
 });
