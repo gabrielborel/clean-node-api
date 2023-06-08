1d8cabfa8430d202087b3b5fe01bced3db67df6f
Gabriel Borel <biel_borel@hotmail.com>
2023-06-08 04:34:58 -0300

    feat: ensure SignUpController returns an error if no email is provided

--- src/presentations/controllers/signup.ts
+++ src/presentations/controllers/signup.ts
@@ -1,9 +1,16 @@
 export class SignUpController {
   handle(request: any): any {
-    if (request.body.name === "") {
+    if (!request.body.name) {
       return {
         statusCode: 400,
         body: new Error("name is required"),
+      };
+    }
+
+    if (!request.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("email is required"),
       };
     }
   }
