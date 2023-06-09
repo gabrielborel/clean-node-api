868c1d5baed119f8bb6da8df677adbf878f06930
Gabriel Borel <biel_borel@hotmail.com>
2023-06-09 09:48:43 -0300

    feat: ensure MongoAccountRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,21 @@
+import { CreateAccountRepository } from "../../../../data/protocols/create-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { CreateAccountModel } from "../../../../domain/use-cases/create-account";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class MongoAccountRepository implements CreateAccountRepository {
+  async create(account: CreateAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(account);
+    const createdAccount = await accountCollection.findOne({
+      _id: result.insertedId,
+    });
+    if (!createdAccount) throw new Error("account not found");
+    return {
+      id: createdAccount._id.toString(),
+      name: createdAccount.name,
+      email: createdAccount.email,
+      password: createdAccount.password,
+    };
+  }
+}
