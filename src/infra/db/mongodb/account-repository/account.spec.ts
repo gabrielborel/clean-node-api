640b217f5365ddd43c054b19cc2682436c6b51f4
Gabriel Borel <biel_borel@hotmail.com>
2023-06-09 09:48:50 -0300

    test: ensure MongoAccountRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { MongoAccountRepository } from "./account";
+
+describe("MongoDB Account Repository", () => {
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      console.log(process.env);
+      await MongoHelper.connect(process.env.MONGO_URL);
+    }
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("should return an account on success", async () => {
+    const sut = new MongoAccountRepository();
+    const accountData = {
+      name: "any_name",
+      email: "valid_email@mail.com",
+      password: "any_password",
+    };
+    const account = await sut.create(accountData);
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe(accountData.name);
+    expect(account.email).toBe(accountData.email);
+    expect(account.password).toBe(accountData.password);
+  });
+});
