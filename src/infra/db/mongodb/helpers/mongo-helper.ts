d8da6dd09056e68920d0047d226ee91f76de956c
Gabriel Borel <biel_borel@hotmail.com>
2023-06-10 01:36:23 -0300

    refactor: coverage collection and mongodb connection

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,9 +4,7 @@ export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect(url: string): Promise<void> {
-    if (process.env.MONGO_URL) {
-      this.client = await MongoClient.connect(url, {});
-    }
+    this.client = await MongoClient.connect(url, {});
   },
 
   async disconnect(): Promise<void> {
