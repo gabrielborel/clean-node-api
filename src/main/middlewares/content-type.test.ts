c0288739a8bbfdc39f6de8fca84242ce44aaf4c3
Gabriel Borel <biel_borel@hotmail.com>
2023-06-09 21:05:22 -0300

    feat: ensure requests return json as default content-type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { app } from "../config/app";
+
+describe("Content-Type Middleware", () => {
+  test("should return default content-type as json", async () => {
+    app.get("/test_content-type", (req, res) => {
+      res.send();
+    });
+    await request(app).get("/test_content-type").expect("content-type", /json/);
+  });
+
+  test("should return content-type as xml when forced", async () => {
+    app.get("/test_content-type-xml", (req, res) => {
+      res.type("xml");
+      res.send();
+    });
+    await request(app)
+      .get("/test_content-type-xml")
+      .expect("content-type", /xml/);
+  });
+});
