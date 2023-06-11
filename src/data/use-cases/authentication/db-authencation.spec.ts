1017dde8ed35ef9eab311256e555c62b8cc4543c
Gabriel Borel <biel_borel@hotmail.com>
2023-06-11 19:09:06 -0300

    refactor: create some factory methods and SutType refactor

--- src/data/use-cases/authentication/db-authencation.spec.ts
+++ src/data/use-cases/authentication/db-authencation.spec.ts
@@ -2,27 +2,33 @@ import { describe, test, vi, expect } from "vitest";
 import { AccountModel } from "../create-account/db-create-account-protocols";
 import { FindAccountByEmailRepository } from "../../protocols/find-account-by-email-repository";
 import { DbAuthentication } from "./db-authentication";
+import { AuthenticationModel } from "../../../domain/use-cases/authentication";
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "valid_id",
+  name: "valid_name",
+  email: "any_email@mail.com",
+  password: "hashed_password",
+});
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: "any_email@mail.com",
+  password: "any_password",
+});
 
 const makeFindAccountByEmailRepository = (): FindAccountByEmailRepository => {
   class FindAccountByEmailRepositoryStub
     implements FindAccountByEmailRepository
   {
     async find(email: string): Promise<AccountModel> {
-      return new Promise((resolve) =>
-        resolve({
-          id: "valid_id",
-          name: "valid_name",
-          email: "any_email@mail.com",
-          password: "hashed_password",
-        })
-      );
+      return new Promise((resolve) => resolve(makeFakeAccount()));
     }
   }
   return new FindAccountByEmailRepositoryStub();
 };
 
 interface SutType {
-  sut: any;
+  sut: DbAuthentication;
   findAccountByEmailRepositoryStub: FindAccountByEmailRepository;
 }
 
@@ -39,10 +45,7 @@ describe("DbAuthentication UseCase", () => {
   test("should call FindAccountByEmailRepository with correct values", async () => {
     const { sut, findAccountByEmailRepositoryStub } = makeSut();
     const findSpy = vi.spyOn(findAccountByEmailRepositoryStub, "find");
-    await sut.auth({
-      email: "any_email@mail.com",
-      password: "any_password",
-    });
+    await sut.auth(makeFakeAuthentication());
     expect(findSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
