8b2539ad6120bc5fab9c8f576247ec91ec33a8aa
Gabriel Borel <biel_borel@hotmail.com>
2023-06-11 19:47:11 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -2,15 +2,21 @@ import {
   Authentication,
   AuthenticationModel,
 } from "../../../domain/use-cases/authentication";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 import { FindAccountByEmailRepository } from "../../protocols/db/find-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
   constructor(
-    private readonly findAccountByEmailRepository: FindAccountByEmailRepository
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth(data: AuthenticationModel): Promise<string | null> {
-    await this.findAccountByEmailRepository.find(data.email);
+    const account = await this.findAccountByEmailRepository.find(data.email);
+    if (!account) {
+      return null;
+    }
+    await this.hashComparer.compare(data.password, account.password);
     return null;
   }
 }
