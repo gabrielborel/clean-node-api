9df0d7b2c8708b2ddfb6806641f7ff462839a6f4
Gabriel Borel <biel_borel@hotmail.com>
2023-06-14 20:50:29 -0300

    feat: ensure AccountMongoRepository returns an account on findByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,12 @@
 import { CreateAccountRepository } from "../../../../data/protocols/db/create-account-repository";
+import { FindAccountByEmailRepository } from "../../../../data/protocols/db/find-account-by-email-repository";
 import { AccountModel } from "../../../../domain/models/account";
 import { CreateAccountModel } from "../../../../domain/use-cases/create-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class MongoAccountRepository implements CreateAccountRepository {
+export class MongoAccountRepository
+  implements CreateAccountRepository, FindAccountByEmailRepository
+{
   async create(account: CreateAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
     const result = await accountCollection.insertOne(account);
@@ -12,5 +15,12 @@ export class MongoAccountRepository implements CreateAccountRepository {
     });
     if (!createdAccount) throw new Error("account not found");
     return MongoHelper.toDomain<AccountModel>(createdAccount);
+  }
+
+  async findByEmail(email: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+    if (!account) return null;
+    return MongoHelper.toDomain<AccountModel>(account);
   }
 }
