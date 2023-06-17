3d3713a0b7d56fed35e3da68274cd55cb80cd739
Gabriel Borel <biel_borel@hotmail.com>
2023-06-17 14:54:00 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentations/controllers/signup/signup-controller-protocols.ts
+++ src/presentations/controllers/signup/signup-controller-protocols.ts
@@ -3,3 +3,4 @@ export * from "../../../domain/use-cases/create-account";
 export * from "../../../domain/models/account";
 export * from "../../protocols/validation";
 export * from "../../protocols/email-validator";
+export * from "../../../domain/use-cases/authentication";
--- src/presentations/controllers/signup/signup-controller.ts
+++ src/presentations/controllers/signup/signup-controller.ts
@@ -3,6 +3,7 @@ import {
   created,
   serverError,
 } from "../../helpers/http/http-helper";
+import { Authentication } from "../signin/signin-controller-protocols";
 import {
   Controller,
   CreateAccount,
@@ -14,7 +15,8 @@ import {
 export class SignUpController implements Controller {
   constructor(
     private readonly createAccount: CreateAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
@@ -24,7 +26,18 @@ export class SignUpController implements Controller {
         return badRequest(error);
       }
 
-      const account = await this.createAccount.create(request.body);
+      const { name, email, password } = request.body;
+      const account = await this.createAccount.create({
+        name,
+        email,
+        password,
+      });
+
+      const accessToken = await this.authentication.auth({
+        email,
+        password,
+      });
+
       return created(account);
     } catch (error) {
       return serverError(error as Error);
