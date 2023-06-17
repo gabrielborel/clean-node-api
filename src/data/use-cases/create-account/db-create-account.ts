354e166f0f8a45b578397ae45729aa275393d2a2
Gabriel Borel <biel_borel@hotmail.com>
2023-06-17 15:55:58 -0300

    feat: ensure DbCreateAccount calls FindAccountByEmailRepository with correct email

--- src/data/use-cases/create-account/db-create-account.ts
+++ src/data/use-cases/create-account/db-create-account.ts
@@ -3,16 +3,19 @@ import {
   CreateAccount,
   CreateAccountModel,
   CreateAccountRepository,
+  FindAccountByEmailRepository,
   Hasher,
 } from "./db-create-account-protocols";
 
 export class DbCreateAccount implements CreateAccount {
   constructor(
     private readonly hasher: Hasher,
-    private readonly createAccountRepository: CreateAccountRepository
+    private readonly createAccountRepository: CreateAccountRepository,
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository
   ) {}
 
-  async create(accountData: CreateAccountModel): Promise<AccountModel> {
+  async create(accountData: CreateAccountModel): Promise<AccountModel | null> {
+    await this.findAccountByEmailRepository.findByEmail(accountData.email);
     const hashedPassword = await this.hasher.hash(accountData.password);
     const account = Object.assign({}, accountData, {
       password: hashedPassword,
