f3070855a39d6271bd4fbcdd49de5032b555c16e
Gabriel Borel <biel_borel@hotmail.com>
2023-06-16 21:38:32 -0300

    feat: add SignIn route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -10,11 +10,15 @@ import {
   afterAll,
 } from "vitest";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
 
 const timeout = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-describe("Login Routes", () => {
+let accountCollection: Collection;
+
+describe("Login Routes", async () => {
   beforeAll(async () => {
     const mongo = await MongoMemoryServer.create();
     await MongoHelper.connect(mongo.getUri());
@@ -25,7 +29,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -35,7 +39,7 @@ describe("Login Routes", () => {
        * IDK why but this timeout is necessary, if not, the test will return 404
        * like the application hasn't build yet and the test make the request
        */
-      await timeout(100);
+      await timeout(200);
       const response = await request(app).post("/api/signup").send({
         name: "Any Name",
         email: "any_email@mail.com",
@@ -46,6 +50,27 @@ describe("Login Routes", () => {
       expect(response.body.name).toBe("Any Name");
       expect(response.body.email).toBe("any_email@mail.com");
       expect(response.body.password).toBeTruthy();
+    });
+  });
+
+  describe("POST /signin", () => {
+    test("should return success on login", async () => {
+      const password = await hash("any password", 12);
+      await accountCollection.insertOne({
+        name: "Any Name",
+        email: "any_email@mail.com",
+        password,
+      });
+      /**
+       * IDK why but this timeout is necessary, if not, the test will return 404
+       * like the application hasn't build yet and the test make the request
+       */
+      // await timeout(200);
+      const response = await request(app).post("/api/signin").send({
+        email: "any_email@mail.com",
+        password: "any password",
+      });
+      expect(response.status).toBe(200);
     });
   });
 });
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { makeSignUpController } from "../factories/signup/signup-factory";
 import { adaptRoute } from "../adapters/express-route-adapter";
+import { makeSignUpController } from "../factories/signup/signup-factory";
+import { makeSignInController } from "../factories/signin/signin-factory";
 
 export default (router: Router) => {
   router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/signin", adaptRoute(makeSignInController()));
 };
