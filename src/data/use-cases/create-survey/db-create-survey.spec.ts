841e8c61da7c591fe9234cad7d7a8b343e56f260
Gabriel Borel <biel_borel@hotmail.com>
2023-06-18 11:21:28 -0300

    test: ensure DbCreateSurvey calls CreateSurveyRepository with correct values

---
+++ src/data/use-cases/create-survey/db-create-survey.spec.ts
@@ -0,0 +1,46 @@
+import { describe, test, vi, expect } from "vitest";
+import { DbCreateSurvey } from "./db-create-survey";
+import {
+  CreateSurveyModel,
+  CreateSurveyRepository,
+} from "./db-create-survey-protocols";
+
+const makeCreateSurveyRepositoryStub = (): CreateSurveyRepository => {
+  class CreateSurveyRepositoryStub implements CreateSurveyRepository {
+    async create(data: CreateSurveyModel): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+  return new CreateSurveyRepositoryStub();
+};
+
+const makeFakeSurveyData = (): CreateSurveyModel => ({
+  question: "any_question",
+  answers: [
+    {
+      image: "any_image",
+      answer: "any_answer",
+    },
+  ],
+});
+
+interface SutType {
+  sut: DbCreateSurvey;
+  createSurveyRepositoryStub: CreateSurveyRepository;
+}
+
+const makeSut = (): SutType => {
+  const createSurveyRepositoryStub = makeCreateSurveyRepositoryStub();
+  const sut = new DbCreateSurvey(createSurveyRepositoryStub);
+  return { sut, createSurveyRepositoryStub };
+};
+
+describe("DbCreateSurvey Use Case", () => {
+  test("should call CreateSurveyRepository with correct values", async () => {
+    const { sut, createSurveyRepositoryStub } = makeSut();
+    const createSpy = vi.spyOn(createSurveyRepositoryStub, "create");
+    const surveyData = makeFakeSurveyData();
+    await sut.create(surveyData);
+    expect(createSpy).toHaveBeenCalledWith(surveyData);
+  });
+});
