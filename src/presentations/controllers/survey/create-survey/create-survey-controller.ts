0634a4fb52dbd75acfba220e3b9ce701786da4dc
Gabriel Borel <biel_borel@hotmail.com>
2023-06-18 10:47:10 -0300

    feat: ensure CreateSurveyController calls CreateSurvey use-case with correct values

--- src/presentations/controllers/survey/create-survey/create-survey-controller.ts
+++ src/presentations/controllers/survey/create-survey/create-survey-controller.ts
@@ -4,16 +4,26 @@ import {
   HttpRequest,
   Controller,
   HttpResponse,
+  CreateSurvey,
 } from "./create-survey-controller-protocols";
 
 export class CreateSurveyController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly createSurvey: CreateSurvey
+  ) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(request.body);
     if (error) {
       return badRequest(error);
     }
+
+    const { question, answers } = request.body;
+    await this.createSurvey.create({
+      question,
+      answers,
+    });
 
     return ok({});
   }
