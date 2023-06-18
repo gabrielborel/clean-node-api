92c3e43376493edb465543062564645193f53521
Gabriel Borel <biel_borel@hotmail.com>
2023-06-18 11:47:08 -0300

    test: ensure MongoSurveyRepository create a new survey on MongoSurveyRepository.create success

---
+++ src/infra/db/mongodb/survey/mongo-survey-repository.test.ts
@@ -0,0 +1,53 @@
+import {
+  test,
+  describe,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+} from "vitest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { MongoHelper } from "../helpers/mongo-helper";
+import { MongoSurveyRepository } from "./mongo-survey-repository";
+import { Collection } from "mongodb";
+
+const makeSut = (): MongoSurveyRepository => {
+  return new MongoSurveyRepository();
+};
+
+let surveyCollection: Collection;
+
+describe.only("MongoDB Survey Repository", () => {
+  beforeAll(async () => {
+    const mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    await MongoHelper.connect(uri);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection("surveys");
+    await surveyCollection.deleteMany({});
+  });
+
+  test("should create a survey on success", async () => {
+    const sut = makeSut();
+    await sut.create({
+      question: "any_question",
+      answers: [
+        {
+          image: "any_image",
+          answer: "any_answer",
+        },
+        {
+          answer: "other_answer",
+        },
+      ],
+    });
+    const survey = await surveyCollection.findOne({ question: "any_question" });
+    expect(survey).toBeTruthy();
+  });
+});
