0e884b98c62178a0c7304642a23a9ae54504f446
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 08:53:00 -0300

    test: ensure DbFindAccountByAccessToken calls Decrypter with correct value

---
+++ src/data/use-cases/find-account-by-access-token/db-find-account-by-access-token.spec.ts
@@ -0,0 +1,32 @@
+import { describe, test, vi, expect } from "vitest";
+import { Decrypter } from "../../protocols/criptography/decrypter";
+import { DbFindAccountByAccessToken } from "./db-find-account-by-access-token";
+
+const makeDecrypterStub = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(value: string): Promise<string> {
+      return "haha";
+    }
+  }
+  return new DecrypterStub();
+};
+
+interface SutType {
+  sut: DbFindAccountByAccessToken;
+  decrypterStub: Decrypter;
+}
+
+const makeSut = (): SutType => {
+  const decrypter = makeDecrypterStub();
+  const sut = new DbFindAccountByAccessToken(decrypter);
+  return { sut, decrypterStub: decrypter };
+};
+
+describe("DbFindAccountByAccessToken Use Case", () => {
+  test("shoudld call Decrypter with correct values", () => {
+    const { sut, decrypterStub } = makeSut();
+    const decryptSpy = vi.spyOn(decrypterStub, "decrypt");
+    sut.find("any_token");
+    expect(decryptSpy).toHaveBeenCalledWith("any_token");
+  });
+});
