512d02685db533b7965a010bf795f21acff804f3
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 09:04:34 -0300

    feat: ensure DbFindAccountByAccessToken calls FindAccountByAccessTokenRepository with correct values

---
+++ src/data/protocols/db/account/find-account-by-access-token-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from "../../../../domain/models/account";
+
+export interface FindAccountByAccessTokenRepository {
+  findByAccessToken(token: string, role?: string): Promise<AccountModel | null>;
+}
--- src/data/use-cases/find-account-by-access-token/db-find-account-by-access-token.ts
+++ src/data/use-cases/find-account-by-access-token/db-find-account-by-access-token.ts
@@ -1,15 +1,25 @@
 import { FindAccountByAccessToken } from "../../../domain/use-cases/find-account-by-access-token";
 import { Decrypter } from "../../protocols/criptography/decrypter";
+import { FindAccountByAccessTokenRepository } from "../../protocols/db/account/find-account-by-access-token-repository";
 import { AccountModel } from "../create-account/db-create-account-protocols";
 
 export class DbFindAccountByAccessToken implements FindAccountByAccessToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly findAccountByAccessTokenRepository: FindAccountByAccessTokenRepository
+  ) {}
 
   async find(
     accessToken: string,
     role?: string | undefined
   ): Promise<AccountModel | null> {
-    await this.decrypter.decrypt(accessToken);
+    const decryptedAccessToken = await this.decrypter.decrypt(accessToken);
+    if (!decryptedAccessToken) return null;
+
+    await this.findAccountByAccessTokenRepository.findByAccessToken(
+      accessToken,
+      role
+    );
     return null;
   }
 }
