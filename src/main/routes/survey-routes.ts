52907ff99cef913fba4c8400a373780f1563d67e
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 09:42:49 -0300

    refactor: move create survey factories to its own folder

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
-import { makeCreateSurveyController } from "../factories/controllers/survey/create-survey-controller-factory";
+import { makeCreateSurveyController } from "../factories/controllers/survey/create-survey/create-survey-controller-factory";
 
 export default (router: Router) => {
   router.post("/surveys", adaptRoute(makeCreateSurveyController()));
