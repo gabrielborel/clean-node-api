e8d6f7ebd650d48bac6f74a55c2cc60a06a94a73
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 10:09:33 -0300

    refactor: moving files to its own folder

--- src/main/factories/controllers/signin/signin-controller-factory.ts
+++ src/main/factories/controllers/signin/signin-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignInController } from "../../../../presentations/controllers/login/signin/signin-controller";
 import { Controller } from "../../../../presentations/protocols";
 import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator-factory";
-import { makeDbAuthentication } from "../../use-cases/authentication/authentication-factory";
+import { makeDbAuthentication } from "../../use-cases/account/authentication/authentication-factory";
 import { makeSignInValidation } from "./signin-validation-factory";
 
 export const makeSignInController = (): Controller => {
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from "../../../../presentations/controllers/login/signup/signup-controller";
 import { Controller } from "../../../../presentations/protocols";
 import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator-factory";
-import { makeDbCreateAccount } from "../../use-cases/account/create-account-factory";
-import { makeDbAuthentication } from "../../use-cases/authentication/authentication-factory";
+import { makeDbCreateAccount } from "../../use-cases/account/create-account/create-account-factory";
+import { makeDbAuthentication } from "../../use-cases/account/authentication/authentication-factory";
 import { makeSignUpValidation } from "./signup-validation-factory";
 
 export const makeSignUpController = (): Controller => {
