10f97c54d00539900a7d2231194ccebe17614137
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 08:56:31 -0300

    test: ensure DbFindAccountByAccessToken returns null if Decrypter returns null

--- src/data/use-cases/find-account-by-access-token/db-find-account-by-access-token.spec.ts
+++ src/data/use-cases/find-account-by-access-token/db-find-account-by-access-token.spec.ts
@@ -4,7 +4,7 @@ import { DbFindAccountByAccessToken } from "./db-find-account-by-access-token";
 
 const makeDecrypterStub = (): Decrypter => {
   class DecrypterStub implements Decrypter {
-    async decrypt(value: string): Promise<string> {
+    async decrypt(value: string): Promise<string | null> {
       return "haha";
     }
   }
@@ -26,7 +26,16 @@ describe("DbFindAccountByAccessToken Use Case", () => {
   test("should call Decrypter with correct values", async () => {
     const { sut, decrypterStub } = makeSut();
     const decryptSpy = vi.spyOn(decrypterStub, "decrypt");
-    await sut.find("any_token");
+    await sut.find("any_token", "any_role");
     expect(decryptSpy).toHaveBeenCalledWith("any_token");
+  });
+
+  test("should return null if Decrypter returns null", async () => {
+    const { sut, decrypterStub } = makeSut();
+    vi.spyOn(decrypterStub, "decrypt").mockReturnValueOnce(
+      new Promise((resolve) => resolve(null))
+    );
+    const account = await sut.find("any_token");
+    expect(account).toBeNull();
   });
 });
