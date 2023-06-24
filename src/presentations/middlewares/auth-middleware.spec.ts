05079acc158a9dc27f4004a3996650435dce508a
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 08:19:01 -0300

    test: ensure AuthMiddleware calls FindAccountByAccessToken with correct acessToken

--- src/presentations/middlewares/auth-middleware.spec.ts
+++ src/presentations/middlewares/auth-middleware.spec.ts
@@ -1,8 +1,30 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { HttpRequest } from "../protocols/http";
 import { AccessDeniedError } from "../errors";
 import { forbidden } from "../helpers/http/http-helper";
 import { AuthMiddleware } from "./auth-middleware";
+import { FindAccountByAccessToken } from "../../domain/use-cases/find-account-by-access-token";
+import { AccountModel } from "../../domain/models/account";
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "valid_id",
+  name: "valid_name",
+  email: "any_email@mail.com",
+  password: "hashed_password",
+  accessToken: "valid_token",
+});
+
+const makeFindAccountByAccessTokenStub = () => {
+  class FindAccountByAccessTokenStub implements FindAccountByAccessToken {
+    async find(
+      accessToken: string,
+      role?: string | undefined
+    ): Promise<AccountModel | null> {
+      return makeFakeAccount();
+    }
+  }
+  return new FindAccountByAccessTokenStub();
+};
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: {},
@@ -10,11 +32,13 @@ const makeFakeRequest = (): HttpRequest => ({
 
 interface SutType {
   sut: AuthMiddleware;
+  findAccountByAccessTokenStub: FindAccountByAccessToken;
 }
 
 const makeSut = (): SutType => {
-  const sut = new AuthMiddleware();
-  return { sut };
+  const findAccountByAccessToken = makeFindAccountByAccessTokenStub();
+  const sut = new AuthMiddleware(findAccountByAccessToken);
+  return { sut, findAccountByAccessTokenStub: findAccountByAccessToken };
 };
 
 describe("Auth Middleware", () => {
@@ -23,5 +47,17 @@ describe("Auth Middleware", () => {
     const httpRequest = makeFakeRequest();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  test("should call FindAccountByAccessToken with correct accessToken", async () => {
+    const { sut, findAccountByAccessTokenStub } = makeSut();
+    const httpRequest = makeFakeRequest();
+    httpRequest.headers["x-access-token"] = "any_token";
+    const findAccountByAccessTokenSpy = vi.spyOn(
+      findAccountByAccessTokenStub,
+      "find"
+    );
+    await sut.handle(httpRequest);
+    expect(findAccountByAccessTokenSpy).toHaveBeenCalledWith("any_token");
   });
 });
