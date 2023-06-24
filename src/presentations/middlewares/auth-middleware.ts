f2b0d2172971356d5565abc0e94c980e807a71be
Gabriel Borel <biel_borel@hotmail.com>
2023-06-24 08:32:08 -0300

    feat: ensure AuthMiddleware returns 500 if FindAccountByAccessToken throws

--- src/presentations/middlewares/auth-middleware.ts
+++ src/presentations/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { FindAccountByAccessToken } from "../../domain/use-cases/find-account-by-access-token";
 import { AccessDeniedError } from "../errors";
-import { forbidden, ok } from "../helpers/http/http-helper";
+import { forbidden, ok, serverError } from "../helpers/http/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols";
 import { Middleware } from "../protocols/middleware";
 
@@ -10,12 +10,16 @@ export class AuthMiddleware implements Middleware {
   ) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const accessToken = request.headers?.["x-access-token"];
-    if (!accessToken) return forbidden(new AccessDeniedError());
+    try {
+      const accessToken = request.headers?.["x-access-token"];
+      if (!accessToken) return forbidden(new AccessDeniedError());
 
-    const account = await this.findAccountByAccessToken.find(accessToken);
-    if (!account) return forbidden(new AccessDeniedError());
+      const account = await this.findAccountByAccessToken.find(accessToken);
+      if (!account) return forbidden(new AccessDeniedError());
 
-    return ok({ accountId: account.id });
+      return ok({ accountId: account.id });
+    } catch (error) {
+      return serverError(error as Error);
+    }
   }
 }
