9574eeca988ce9ff46412ac954e63e7e75902e68
Gabriel Borel <biel_borel@hotmail.com>
2023-06-25 13:00:46 -0300

    feat: ensure MongoSurveyRepository find all surveys

--- src/infra/db/mongodb/survey/mongo-survey-repository.ts
+++ src/infra/db/mongodb/survey/mongo-survey-repository.ts
@@ -1,10 +1,20 @@
+import { FindSurveysRepository } from "../../../../data/protocols/db/survey/find-surveys-repository";
 import { CreateSurveyRepository } from "../../../../data/use-cases/create-survey/db-create-survey-protocols";
+import { SurveyModel } from "../../../../domain/models/survey";
 import { CreateSurveyModel } from "../../../../domain/use-cases/create-survey";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class MongoSurveyRepository implements CreateSurveyRepository {
+export class MongoSurveyRepository
+  implements CreateSurveyRepository, FindSurveysRepository
+{
   async create(data: CreateSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(data);
+  }
+
+  async findAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveys = await surveyCollection.find().toArray();
+    return surveys.map((survey) => MongoHelper.toDomain<SurveyModel>(survey));
   }
 }
