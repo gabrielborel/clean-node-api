0a16bc7090742a971c51dd2f4bed7360fc8486a4
Gabriel Borel <biel_borel@hotmail.com>
2023-07-08 12:27:31 -0300

    fix: some fixes

--- src/domain/use-cases/create-account.ts
+++ src/domain/use-cases/create-account.ts
@@ -1,6 +1,6 @@
 import { AccountModel } from "@/domain/models/account";
 
-export type CreateAccountModel = Omit<AccountModel, "id">;
+export type CreateAccountModel = Omit<AccountModel, "id" | "accessToken">;
 
 export interface CreateAccount {
   create(account: CreateAccountModel): Promise<AccountModel | null>;
