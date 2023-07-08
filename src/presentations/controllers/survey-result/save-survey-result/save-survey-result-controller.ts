6c445535a08ba3ac9cbe0cfe86027a7a8e22c36e
Gabriel Borel <biel_borel@hotmail.com>
2023-07-08 18:40:55 -0300

    feat: ensure SaveSurveyResultController returns 500 if FindSurveyById throws

--- src/presentations/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentations/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -6,19 +6,24 @@ import {
   HttpResponse,
   forbidden,
   ok,
+  serverError,
 } from "./save-survey-result-protocols";
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly findSurveyById: FindSurveyById) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = request.params;
+    try {
+      const { surveyId } = request.params;
 
-    const survey = await this.findSurveyById.findById(surveyId);
-    if (!survey) {
-      return forbidden(new InvalidParamError("surveyId"));
+      const survey = await this.findSurveyById.findById(surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError("surveyId"));
+      }
+
+      return ok(survey);
+    } catch (error) {
+      return serverError(error as Error);
     }
-
-    return ok(survey);
   }
 }
