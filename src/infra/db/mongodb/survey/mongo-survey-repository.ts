99e8a8e93f536413fa5cafc1db3eeb4d109d1b4f
Gabriel Borel <biel_borel@hotmail.com>
2023-07-08 17:16:54 -0300

    feat: ensure MongoSurveyRepository find a survey by id

--- src/infra/db/mongodb/survey/mongo-survey-repository.ts
+++ src/infra/db/mongodb/survey/mongo-survey-repository.ts
@@ -1,11 +1,16 @@
 import { FindSurveysRepository } from "@/data/protocols/db/survey/find-surveys-repository";
 import { CreateSurveyRepository } from "@/data/use-cases/create-survey/db-create-survey-protocols";
+import { FindSurveyByIdRepository } from "@/data/use-cases/find-survey-by-id/db-find-survey-by-id-protocols";
 import { SurveyModel } from "@/domain/models/survey";
 import { CreateSurveyModel } from "@/domain/use-cases/create-survey";
 import { MongoHelper } from "../helpers/mongo-helper";
+import { ObjectId } from "mongodb";
 
 export class MongoSurveyRepository
-  implements CreateSurveyRepository, FindSurveysRepository
+  implements
+    CreateSurveyRepository,
+    FindSurveysRepository,
+    FindSurveyByIdRepository
 {
   async create(data: CreateSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
@@ -16,5 +21,12 @@ export class MongoSurveyRepository
     const surveyCollection = await MongoHelper.getCollection("surveys");
     const surveys = await surveyCollection.find().toArray();
     return surveys.map((survey) => MongoHelper.toDomain<SurveyModel>(survey));
+  }
+
+  async findById(id: string): Promise<SurveyModel | null> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) });
+    if (!survey) return null;
+    return MongoHelper.toDomain<SurveyModel>(survey);
   }
 }
