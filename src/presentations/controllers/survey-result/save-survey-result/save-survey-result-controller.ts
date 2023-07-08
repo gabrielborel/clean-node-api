a3283c2d7a8b91017b0ffa9d93b9d6b41514237b
Gabriel Borel <biel_borel@hotmail.com>
2023-07-08 18:37:04 -0300

    feat: ensure SaveSurveyResultController returns 403 if FindSurveyById returns null

--- src/presentations/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentations/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,8 +1,10 @@
+import { InvalidParamError } from "@/presentations/errors";
 import {
   Controller,
   FindSurveyById,
   HttpRequest,
   HttpResponse,
+  forbidden,
   ok,
 } from "./save-survey-result-protocols";
 
@@ -13,6 +15,9 @@ export class SaveSurveyResultController implements Controller {
     const { surveyId } = request.params;
 
     const survey = await this.findSurveyById.findById(surveyId);
+    if (!survey) {
+      return forbidden(new InvalidParamError("surveyId"));
+    }
 
     return ok(survey);
   }
